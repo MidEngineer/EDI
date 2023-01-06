482f65c1c009c0e2b9dc5a57a25020316a4ba5c5
MidEngineer <kilopig2@gmail.com>
2023-01-06 09:55:59 -0500

    pretty square

---
+++ CreativeCoding/Grid/sketch.js
@@ -0,0 +1,29 @@
+function setup() { //runs once
+  createCanvas(720,720);
+  background(255, 0, 255);
+}
+
+function draw() { //loopty loop
+  fill(255,0,0);
+  strokeWeight(8);
+  translate(60,60);
+  quad(0,0,
+       300, 0,
+       300, 300,
+       0, 300);
+
+  quad(300, 0,
+      600, 0,
+      600, 300,
+      300, 300);
+
+  quad(0, 300,
+      300, 300,
+      300, 600,
+      0, 600);
+
+  quad(300, 300,
+      600, 300,
+      600, 600,
+      300, 600);
+}
